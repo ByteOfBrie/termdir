79dc751a4722017d95d1a7dbbdf9159231b988a5
Brie <brie@brie.gay>
2016-03-31 10:14:43 -0400

    licensed it under the GPL v3

--- termdir.py
+++ termdir.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 '''
-find the current directory of a shell's process in i3 window manager
-works for shells like konsole that support tabs
+Copyright 2016 Brian Van Rosendale
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import i3
